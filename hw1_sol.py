facfea7e92968fd17ab9cfa49f547b9ebb9a4cad
Han-16 <haanbk16@gmail.com>
2023-04-15 10:47:41 +0900

    first commit

---
+++ hw1_sol.py
@@ -0,0 +1,26 @@
+import sys
+from socket import *
+
+# 커맨드라인에서 서버 이름과 포트 번호를 얻음
+serverName = sys.argv[1]
+serverPort = int(sys.argv[2])
+
+# 클라이언트 소켓 생성하고 서버에 연결
+clientSocket = socket(AF_INET, SOCK_STREAM)
+clientSocket.connect((serverName, serverPort))
+
+sentences = []
+while True:
+    line = input()
+    if not line.strip():
+        break
+    line += "\r\n"
+    sentences.append(line)
+sentences.append("\r\n")
+message = ''.join(sentences)
+
+clientSocket.send(message.encode())
+
+data = clientSocket.recv(1024)
+print('Received', data.decode())
+clientSocket.close()
